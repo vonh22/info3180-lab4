7c4843c6b6696eb16e59227afce4bd998465bdd2
Von Harris <vonharris00@gmail.com>
2024-03-06 14:52:55 -0500

    Exercise 6

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm, UploadForm
 from werkzeug.security import check_password_hash
+
 
 
 ###
@@ -87,6 +88,30 @@ def login():
 def load_user(id):
     return db.session.execute(db.select(UserProfile).filter_by(id=id)).scalar()
 
+
+
+def get_uploaded_images():
+    upload_dir = os.path.join(os.getcwd(), 'uploads')  
+    image_files = []
+
+    for subdir, dirs, files in os.walk(upload_dir):
+        for file in files:
+            if file.endswith('.jpg') or file.endswith('.png'): 
+                image_files.append(file)
+    return image_files
+
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    upload_folder = os.path.join(os.getcwd(), 'uploads')  
+    return send_from_directory(upload_folder, filename)
+
+
+@app.route('/files')
+@login_required 
+def files():
+    images = get_uploaded_images()  
+    return render_template('files.html', images=images)
+
 ###
 # The functions below should be applicable to all Flask apps.
 ###
@@ -99,6 +124,9 @@ def flash_errors(form):
                 getattr(form, field).label.text,
                 error
 ), 'danger')
+            
+
+
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
@@ -122,3 +150,6 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+
